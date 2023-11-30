a750236fa14f1ccaedc55d0d6f89610a1d070d79
Uyen Nguyen <uyen@gmail.com>
2023-11-30 19:46:56 +0700

    Add project files.

---
+++ GameMario/GameMario/CauHoiDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace GameMario
+{
+	public class CauHoiDB
+	{
+		public static DataTable LayDSCauHoi(int mon)
+		{
+			return Database.SelectQuery("select * from CauHoi where MAMON=" + mon);
+			//return Database.SelectQuery("select * from CauHoi where Mon=" + mon);
+		}
+	}
+}
