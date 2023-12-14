0be66f36152abc9c189ef03749337b743cd56a5a
moana <128701096+Wyden13@users.noreply.github.com>
2023-12-14 18:19:25 +0700

    Update CauHoiDB.cs

--- PikachuGame/GameMario/GameMario/CauHoiDB.cs
+++ PikachuGame/GameMario/GameMario/CauHoiDB.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:GameMario/GameMario/CauHoiDB.cs
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,22 +15,4 @@ namespace GameMario
 		}
 	}
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
 
-namespace GameMario
-{
-	public class CauHoiDB
-	{
-		public static DataTable LayDSCauHoi(int mon)
-		{
-			return Database.SelectQuery("select * from CauHoi where MAMON=" + mon);
-		}
-	}
-}
->>>>>>> 061aa2bb68e35f8f1ca9690b87dcf623e192f233:PikachuGame/GameMario/GameMario/CauHoiDB.cs
