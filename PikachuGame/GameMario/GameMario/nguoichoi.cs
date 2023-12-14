858633902a29c650e43ee91f88182b31defbfb3d
moana <128701096+Wyden13@users.noreply.github.com>
2023-12-14 18:21:38 +0700

    Update nguoichoi.cs

--- PikachuGame/GameMario/GameMario/nguoichoi.cs
+++ PikachuGame/GameMario/GameMario/nguoichoi.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:GameMario/GameMario/nguoichoi.cs
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
@@ -34,40 +33,3 @@ namespace GameMario
 		}
 	}
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Media;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GameMario
-{
-	public class nguoichoi
-	{
-		//public string skin;
-		public string ten { get; set; }
-		public int mon { get; set; }
-		public int highest_score { get;set; }
-		public int mang
-		{
-			get; set;
-		}
-		public int diem
-		{
-			get; set;
-		}
-		
-		public nguoichoi(string name, int mon)
-		{
-			this.ten = name;
-			this.mon = mon;
-			this.diem = 0;
-			this.highest_score = 0;
-			this.mang = 3;
-		}
-	}
-}
->>>>>>> 061aa2bb68e35f8f1ca9690b87dcf623e192f233:PikachuGame/GameMario/GameMario/nguoichoi.cs
