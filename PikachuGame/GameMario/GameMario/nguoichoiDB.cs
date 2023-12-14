58e569212adb151d6d144949706bce3f0e3e4b2a
moana <128701096+Wyden13@users.noreply.github.com>
2023-12-14 18:21:54 +0700

    Update nguoichoiDB.cs

--- PikachuGame/GameMario/GameMario/nguoichoiDB.cs
+++ PikachuGame/GameMario/GameMario/nguoichoiDB.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:GameMario/GameMario/nguoichoiDB.cs
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
@@ -16,22 +15,4 @@ namespace GameMario
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GameMario
-{
-    public class nguoichoiDB
-    {
-        public static DataTable LayTTNguoiChoi()
-        {
-            return Database.SelectQuery("select * from Player");
-        }
-    }
-}
->>>>>>> 061aa2bb68e35f8f1ca9690b87dcf623e192f233:PikachuGame/GameMario/GameMario/nguoichoiDB.cs
