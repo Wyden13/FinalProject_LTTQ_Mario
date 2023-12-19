b3883a1c79c973b7bfd09cc908d421c14edfe0f6
moana <128701096+Wyden13@users.noreply.github.com>
2023-12-19 20:49:42 +0700

    Add files via upload

---
+++ PikachuGame/Practice/Practice/MonDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class MonDB
+    {
+        public static DataTable LayDSMon()
+        {
+            return Database.SelectQuery("select * from Mon");
+        }
+    }
+}
